c7c241e13c68d07b782b7a7f12955fb95ab3bd93
Seito0130 <seitourano@gmail.com>
2026-02-24 06:47:47 +0900

    first commit

---
+++ web.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    app.run(debug=True, ssl_context=('cert.pem', 'key.pem'))
