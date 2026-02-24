eda8518084c845b495494b458eb40cc48c6172f1
Seito0130 <seitourano@gmail.com>
2026-02-24 09:41:29 +0900

    fix: ignore venv

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,11 +8,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(debug=True, ssl_context=('cert.pem', 'key.pem'))
-
-import os
-
-port = int(os.environ.get("PORT", 5000))
-
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
