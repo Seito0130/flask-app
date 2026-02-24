45d616e3393564514428f4e45ecf1547935daf6c
Seito0130 <seitourano@gmail.com>
2026-02-24 16:33:40 +0900

    Add files via upload

--- web.py
+++ web.py
@@ -1,51 +1,27 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
 
 app = Flask(__name__, template_folder=os.path.join(os.getcwd(),'templates'),static_folder=os.path.join(os.getcwd(),'static'))
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
-
-
-
-
-
 # --- 便利なアプリ ---
-
-
 
 @app.route("/apps")
 
-
-
 def apps():
-
-
 
     app_list = [
 
 
-
         {"name": "Gemini", "img": "app1.png", "desc": "最強のAIツール！使い勝手抜群。", "stars": "★★★★★", "site_url": "https://gemini.google.com/"},
-
-
 
         {"name": "Capcat", "img": "app2.png", "desc": "動画編集をこれ一本で！", "stars": "★★★★☆", "site_url": "https://www.capcut.com/"}
 
-
-
     ]
 
-
-
     return render_template("apps.html", apps=app_list)
-
-
-
-
-
 
 
 # --- ポイ活ページ ---
@@ -68,7 +44,7 @@ def poikatu():
 
 
 
-        {"name": "QuickPoint", "img": "poi2.png", "desc": "PayPay連携が神！", "stars": "★★★★☆", "site_url": "#"},
+        {"name": "QuickPoint", "img": "poi2.jpg", "desc": "PayPay連携が神！", "stars": "★★★★☆", "site_url": "#"},
 
 
 
@@ -104,25 +80,15 @@ def items():
 
 
 
-        {"name": "shiroのwhite lily", "img": "item1.png", "desc": "コスパ最強の香水！", "stars": "★★★★★", "https://shiro-shiro.jp/item/12722.html?category_from=250": "#"},
+        {"name": "shiroのwhite lily", "img": "item1.jpg", "desc": "コスパ最強の香水！", "stars": "★★★★★", "https://shiro-shiro.jp/item/12722.html?category_from=250": "#"},
 
 
 
-        {"name": "Applewatch", "img": "item2.png", "desc": "モチベ上がる！ スマホをいちいち操作しなくてもこれで簡潔！", "stars": "★★★★★", "https://www.amazon.co.jp/dp/B0FQGP2P41/ref=fs_a_w_2?th=1": "#"}
-
-
+        {"name": "Applewatch", "img": "item2.jpg", "desc": "モチベ上がる！ スマホをいちいち操作しなくてもこれで簡潔！", "stars": "★★★★★", "https://www.amazon.co.jp/dp/B0FQGP2P41/ref=fs_a_w_2?th=1": "#"}
 
     ]
 
-
-
     return render_template("items.html", items=item_list)
-
-
-
-
-
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
